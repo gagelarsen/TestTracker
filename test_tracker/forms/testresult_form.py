d10d5cbb8d34df189aac9c9511046c4cd81f085f
Gage Larsen <gagelarsen53@gmail.com>
2021-11-19 10:12:02 -0700

    update result note based on status

--- test_tracker/forms/testresult_form.py
+++ test_tracker/forms/testresult_form.py
@@ -1,8 +1,6 @@
 from django import forms
 from test_tracker.models.test_result import TestResult
 from bootstrap_modal_forms.forms import BSModalModelForm
-from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
-from bootstrap_datepicker_plus import DatePickerInput
 
 
 class TestResultForm(BSModalModelForm):
